8bf480f797e5a1880e48bbbb106d77e435791cbe
James Jelenko <jamesjelenko@gmail.com>
2024-12-02 20:49:08 -0800

    refactor GUI layout, reoganize and label code

--- gas-widget.py
+++ gas-widget.py
@@ -5,45 +5,50 @@ from config import BASESCAN_API_KEY, ETHERSCAN_API_KEY
 from constants import (BASE_API_URL, ETH_API_URL, WIDGET_HEIGHT, WIDGET_WIDTH)
 
 
-# Function to update gas prices
-def update_prices(client_label_pairs):
+def update_prices(client, label):
+    price = client.fetch_gas_price_for_network()
+    label.config(text=f"{client.explorer_name} Gas Price: {price}")
+"""
     for client, label in client_label_pairs:
         price = client.fetch_gas_price_for_network()
         label.config(text=f"{client.explorer_name} Gas Price: \n{price}")
-
+"""
 
 def main():
-    # Create client instances
     etherscan_client = EtherscanClient("Etherscan", ETH_API_URL, ETHERSCAN_API_KEY)
     basescan_client = BasescanClient("Basescan", BASE_API_URL, BASESCAN_API_KEY)
 
-    # Create GUI window
     root = tk.Tk()
     root.title("Gas Prices")
     root.geometry(f"{WIDGET_WIDTH}x{WIDGET_HEIGHT}")
 
-    # Ethereum Gas Price Label
-    eth_label = tk.Label(root, text="Ethereum Gas Price: Fetching...")
-    eth_label.pack(pady=10)
+	# Eth client GUI elements 
+    eth_frame = tk.Frame(root, relief='sunken', bd=5, padx=0, pady=5)
+    eth_frame.pack(pady=10, fill=tk.BOTH, padx=20) 
 
-    # Base Gas Price Label
-    base_label = tk.Label(root, text="Base Gas Price: Fetching...")
-    base_label.pack(pady=10)
+    eth_label = tk.Label(eth_frame, text="Ethereum Gas Price: Fetching...", justify=tk.LEFT)
+    eth_label.pack(pady=10, fill=tk.BOTH)
+    ethscan_refresh_button = tk.Button(eth_frame, text="Refresh Eth", command=lambda: update_prices(etherscan_client, eth_label))
+    ethscan_refresh_button.pack(fill=tk.BOTH, side=tk.RIGHT)
 
-    # List of tuples (client, label)
+	# Base client GUI elements 
+    base_frame = tk.Frame(root, relief="sunken", bd=5, padx=0, pady=5)
+    base_frame.pack(pady=10, fill=tk.BOTH, padx=20)
+
+    base_label = tk.Label(base_frame, text="Base Gas Price: Fetching...", justify=tk.LEFT)
+    base_label.pack(pady=10, fill=tk.BOTH)
+
+    basescan_refresh_button = tk.Button(base_frame, text="Refresh Base", command=lambda: update_prices(basescan_client, base_label))
+    basescan_refresh_button.pack(fill=tk.BOTH, side=tk.RIGHT)
+    
     client_label_pairs = [
-        (etherscan_client, eth_label),
-        (basescan_client, base_label)
+    (etherscan_client, eth_label),
+    (basescan_client, base_label)
     ]
+    
+    for client, label in client_label_pairs:
+        update_prices(client, label)
 
-    # Refresh Button
-    refresh_button = tk.Button(root, text="Refresh", command=lambda: update_prices(client_label_pairs))
-    refresh_button.pack(pady=10)
-
-    # Initial Update
-    update_prices(client_label_pairs)
-
-    # Run the GUI
     root.mainloop()
 
 
