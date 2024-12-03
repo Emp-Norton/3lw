d538556564a72e2d93cb59caa2bfdf39c02ec484
James Jelenko <jamesjelenko@gmail.com>
2024-12-02 19:12:52 -0800

    add truncation value to constants, reorganize by conceptual grouping and add comments

--- constants.py
+++ constants.py
@@ -1,9 +1,14 @@
-WIDGET_WIDTH=500
-WIDGET_HEIGHT=200
-BASESCAN_NETWORK_ID=8453
-ETHSCAN_NETWORK_ID=1
-ETH_API_URL="https://api.etherscan.io/v2/api"
+# Basescan client API constants 
 BASE_API_URL="https://api.basescan.org/api"
-# TODO: update the templates to handle dynamic module/action inputs - for now just keeping default values hardcoded.
-ETH_QUERY_TEMPLATE="?chainid=1&module=gastracker&action=gasoracle&apikey={}"
-BASE_QUERY_TEMPLATE="?module=proxy&action=eth_gasPrice&apikey={}"
+BASESCAN_NETWORK_ID=8453
+
+# Etherscan client API constants  
+ETH_API_URL="https://api.etherscan.io/v2/api"
+ETHSCAN_NETWORK_ID=1
+
+# General constants to avoid magic numbers 
+GAS_TRUNC_VAL=6
+
+# TK Widget Values
+WIDGET_HEIGHT=200
+WIDGET_WIDTH=1000
