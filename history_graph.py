c5777f871c4ffc0458633cbf556801554ae6c46a
James Jelenko <jamesjelenko@gmail.com>
2024-12-07 15:10:13 -0800

    add starter code for historical graph

---
+++ history_graph.py
@@ -0,0 +1,12 @@
+import tkinter
+
+import numpy as np
+
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+                                               NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
+root = tkinter.Tk()
+root.wm_title("Historical Gas Prices")
