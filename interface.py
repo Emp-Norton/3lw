a4220db95dfb895bf2f7d4a86ca0e93109ad0700
James Jelenko <jamesjelenko@gmail.com>
2024-12-07 15:09:35 -0800

    remove commented code, fix titles

--- interface.py
+++ interface.py
@@ -4,31 +4,24 @@ from constants import (WIDGET_HEIGHT, WIDGET_WIDTH, LABEL__TITLE__FG_COLOR,
 					   LABEL__TITLE__CORNER_RADII, PRICE_FRAME__PADDING)
 
 class TitleLabel(customtkinter.CTkLabel):
-	def __init__(self, master, title_string, width=10, height=10):
-		super().__init__(master, title_string, width=width, height=height, fg_color=LABEL__TITLE__FG_COLOR, corner_radius=LABEL__TITLE__CORNER_RADII)
+	def __init__(self, master, title_string):
+		super().__init__(master, text=title_string,  fg_color=LABEL__TITLE__FG_COLOR, corner_radius=LABEL__TITLE__CORNER_RADII)
 
 class GraphFrame(customtkinter.CTkFrame):
-	def __init__(self, master, title=None, row=1, column=0, columnspan=1):
+	def __init__(self, master, title):
 		super().__init__(master)
 		self.grid_columnconfigure(0, weight=1)
-		self.grid(row=row, column=column, columnspan=columnspan)
-		# self.title = title
-		# self.title = TitleLabel(self, title_string=title)
-		# self.title.grid(row=0, column=0, strick='ew')
-		# self.title = customtkinter.CTkLabel(self,
-		# 									text=self.title,
-		# 									fg_color=LABEL__TITLE__FG_COLOR,
-		# 									corner_radius=LABEL__TITLE__CORNER_RADII)
+		self.grid_rowconfigure(0, weight=1)
+		self.title = TitleLabel(self, title_string=title)
+		self.title.grid(column=0, row=0)
 
 class PriceFrame(customtkinter.CTkFrame):
-	def __init__(self, master, title='test', row=0, column=0):
+	def __init__(self, master, title):
 		super().__init__(master)
 		self.grid_columnconfigure(0, weight=1)
-		# self.grid(row=row, column=column)
-		self.title = customtkinter.CTkLabel(self,
-											text=title,
-											fg_color=LABEL__TITLE__FG_COLOR,
-											corner_radius=int(LABEL__TITLE__CORNER_RADII))
+		self.grid_rowconfigure(0, minsize=250)
+		self.title = TitleLabel(self, title_string=title)
+		self.title.grid(column=0, row=0)
 
 
 
@@ -42,10 +35,10 @@ class AppGUI(customtkinter.CTk):
 
 		self.eth_price_frame = PriceFrame(self, title='Ethereum Price')
 		self.eth_price_frame.grid(row=0, column=0, stick='ewn', padx=PRICE_FRAME__PADDING, pady=PRICE_FRAME__PADDING)
-		self.base_price_frame = PriceFrame(self)
+		self.base_price_frame = PriceFrame(self, title='Base Price')
 		self.base_price_frame.grid(row=0, column=1, sticky='ewn', padx=PRICE_FRAME__PADDING, pady=PRICE_FRAME__PADDING)
 
-		self.historical_graph_frame = GraphFrame(self)
+		self.historical_graph_frame = GraphFrame(self, title='Historical')
 		self.historical_graph_frame.grid(row=1, columnspan=2, sticky='ews', padx=PRICE_FRAME__PADDING, pady=PRICE_FRAME__PADDING)
 
 
